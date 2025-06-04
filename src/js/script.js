982ffe04db0373d880fc8de75f028580eeaab480
Guilherme de Paula <guilhermekuskowski0@gmail.com>
2025-06-04 15:02:45 -0300

    criando slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,13 @@ function toggleMenu() {
     const menu = document.querySelector('.menu');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
+/* ================= SLIDESHOW ================= */
+const slides = ["imagens/slide1.png", "imagens/slide2.png", "imagens/slide3.png"];
+let slideIndex = 0;
+
+function showSlides() {
+    const img = document.getElementById('slide');
+    slideIndex = (slideIndex + 1) % slides.length;
+    img.src = slides[slideIndex];
+}
+setInterval(showSlides, 3000);
